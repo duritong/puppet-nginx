2d7acef104a8300483bc6c4485d05fd99cdc98e6
mh <mh@immerda.ch>
2021-01-24 00:25:53 +0100

    linting

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,44 +1,28 @@
 # base installation
 class nginx::base {
-  package{'nginx':
+  package { 'nginx':
     ensure => installed,
-  }
-
-  file{
-    ['/etc/nginx/include.d','/etc/nginx/vhosts.d' ]:
+  } -> file {
+    default:
+      owner => root,
+      group => 0,
+      mode  => '0644';
+    ['/etc/nginx/include.d','/etc/nginx/vhosts.d']:
       ensure  => directory,
-      owner   => root,
-      group   => 0,
-      mode    => '0644',
       recurse => true,
       purge   => true,
       force   => true,
-      require => Package['nginx'],
       notify  => Service['nginx'];
-  }
-
-  if versioncmp($::operatingsystemmajrelease,'6') > 0 {
-    file{'/etc/nginx/conf.d/virtual.conf':
-      ensure  => file,
-      owner   => root,
-      group   => 0,
-      mode    => '0644',
-      require => Package['nginx'],
-      notify  => Service['nginx'],
-    }
-  }
-
-  file_line{'load_nginx_vhosts':
-    line    => 'include /etc/nginx/vhosts.d/*.conf;',
-    path    => '/etc/nginx/conf.d/virtual.conf',
-    require => Package['nginx'],
-    notify  => Service['nginx'];
-  }
-
-  service{'nginx':
+    '/etc/nginx/conf.d/virtual.conf':
+      ensure => file,
+      notify => Service['nginx'];
+  } -> file_line { 'load_nginx_vhosts':
+    line => 'include /etc/nginx/vhosts.d/*.conf;',
+    path => '/etc/nginx/conf.d/virtual.conf',
+  } ~> service { 'nginx':
     ensure => running,
     enable => true,
-  } ->  exec{'reload_nginx':
+  } ->  exec { 'reload_nginx':
     refreshonly => true,
     command     => '/usr/bin/systemctl reload nginx',
   }

8d1cd52a2a8bc4d2f3ebe057e0207b4b1f28f86b
mh <mh@immerda.ch>
2021-01-24 16:33:48 +0100

    check the nginx config before trying to restart it

--- manifests/base.pp
+++ manifests/base.pp
@@ -4,26 +4,36 @@ class nginx::base {
     ensure => installed,
   } -> file {
     default:
-      owner => root,
-      group => 0,
-      mode  => '0644';
+      owner  => root,
+      group  => 0,
+      mode   => '0644',
+      notify => [Exec['nginx_config_check'], Service['nginx']];
     ['/etc/nginx/include.d','/etc/nginx/vhosts.d']:
       ensure  => directory,
       recurse => true,
       purge   => true,
-      force   => true,
-      notify  => Service['nginx'];
+      force   => true;
     '/etc/nginx/conf.d/virtual.conf':
-      ensure => file,
-      notify => Service['nginx'];
+      ensure => file;
   } -> file_line { 'load_nginx_vhosts':
-    line => 'include /etc/nginx/vhosts.d/*.conf;',
-    path => '/etc/nginx/conf.d/virtual.conf',
+    line   => 'include /etc/nginx/vhosts.d/*.conf;',
+    path   => '/etc/nginx/conf.d/virtual.conf',
+    notify => [Exec['nginx_config_check'], Service['nginx']];
   } ~> service { 'nginx':
     ensure => running,
     enable => true,
   } ->  exec { 'reload_nginx':
     refreshonly => true,
     command     => '/usr/bin/systemctl reload nginx',
+  }
+
+  File <| tag == 'nginx' |> ~> Exec['nginx_config_check']
+  Concat <| tag == 'nginx' |> ~> Exec['nginx_config_check']
+
+  # the file will be purged on next round, thus we will recheck the config
+  exec { 'nginx_config_check':
+    command     => 'nginx -t || (echo "invalid config found when checking" > /etc/nginx/vhosts.d/puppet_invalid_config && exit 1)',
+    refreshonly => true,
+    notify      => Service['nginx'],
   }
 }
